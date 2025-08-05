6d6ef8d6d31a4c4428619a564e61d7a5a013c7be
wesleymusk0 <sacwesley0@gmail.com>
2025-08-04 23:06:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const path = require('path');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
 
+// Corrigir o private_key
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://pedagogia-systematrix.firebaseio.com'
